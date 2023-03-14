d3b8f1760a981ecf82b3b97067cb830d177de1e3
LoganPearce20 <112974521+LoganPearce20@users.noreply.github.com>
2023-03-14 15:18:03 -0500

    Updated distance metrics
    Calculating distance from team to arena for all arenas in their region

--- QueryScript.R
+++ QueryScript.R
@@ -28,26 +28,49 @@ east_coords =
   mutate(distance = distHaversine(c(Tlon, Tlat), c(Alon, Alat))) %>%
   mutate(distance = distance * 0.00062137)
 
+# Power ranking based on region
+average_east_dist <- east_coords %>%
+  group_by(Team) %>%
+  summarize(avg_dist = mean(distance))
+
 west_coords <- read.csv("Data/west region.csv")
 west_coords =
   west_coords %>%
   dplyr::rowwise() %>%
   mutate(distance = distHaversine(c(Tlon, Tlat), c(Alon, Alat))) %>%
-  mutate(distance = distance * 0.00062137)
+  mutate(distance = distance * 0.00062137) %>%
+  arrange(distance)
+
+# Power rankings based on region
+average_west_dist <- west_coords %>%
+  group_by(Team) %>%
+  summarize(avg_dist = mean(distance))
 
 midwest_coords <- read.csv("Data/midwest region.csv")
 midwest_coords =
   midwest_coords %>%
   dplyr::rowwise() %>%
   mutate(distance = distHaversine(c(Tlon, Tlat), c(Alon, Alat))) %>%
-  mutate(distance = distance * 0.00062137)
+  mutate(distance = distance * 0.00062137) %>%
+  arrange(distance)
+
+# Power region based on region
+average_midwest_dist <- midwest_coords %>%
+  group_by(Team) %>%
+  summarize(avg_dist = mean(distance))
 
 south_coords <- read.csv("Data/south region.csv")
 south_coords =
   south_coords %>%
   dplyr::rowwise() %>%
   mutate(distance = distHaversine(c(Tlon, Tlat), c(Alon, Alat))) %>%
-  mutate(distance = distance * 0.00062137)
+  mutate(distance = distance * 0.00062137) %>%
+  arrange(distance)
+
+# Power rankings based on region
+average_south_dist <- south_coords %>%
+  group_by(Team) %>%
+  summarize(avg_dist = mean(distance))
 
 # Combine all regions into one data frame
 combined_team_dist <- rbind(east_coords, west_coords, midwest_coords, south_coords)
